76cbdeb73c26a9b5d453c2c37d3237bbce0dcc91
Derya Baglan <deryabaglan@Deryas-MacBook-Pro.local>
2025-03-01 13:13:12 +0000

    Fifthteenth commit

--- obj/Debug/net8.0/RunClubAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RunClubAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunClubAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1730267aaa5f6b44db729e3ed0dea5ac40ff85de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ff46ccd096e0600e0dc29588525002e2d45565")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunClubAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunClubAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
