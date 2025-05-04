92a66abe5500ce1f5f8113b4939bea33ef315180
deryosssss <w2115152@westminster.ac.uk>
2025-05-04 09:40:53 +0100

    adding index.css

--- backend/obj/Debug/net8.0/RunClubAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RunClubAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunClubAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18ba884430bf9f4ddecffa706373f972aa0f2e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d236b6285008f1f752c4c251b727c18e0a5077fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunClubAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunClubAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
